9a28ea3b268d8081074f97c03e75b5b66140472d
Александр <@DESKTOP-L9V170T>
2025-07-09 21:51:17 +0300

    Добавление общей сущности

---
+++ UniDAL.SQL.Abstractions/Entity/IEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace UniDAL.SQL.Abstractions
+{
+    /// <summary>
+    /// Базовый интерфейс для всех сущностей
+    /// </summary>
+    /// <typeparam name="TKey">Тип идентификатора сущности</typeparam>
+    public interface IEntity<TKeyType>
+    {
+        // <summary>
+        /// Уникальный идентификатор сущности
+        /// </summary>
+        TKeyType Id { get; set; }
+    }
+}
